61f5b4a64422d3387adc909ffaef463b833342b9
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2014-02-02 00:11:21 -0700

    Solved problem 10 (poorly).

---
+++ source/010/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdint>
+using namespace std;
+
+bool IsPrime(int64_t n)
+{
+    bool result = false;
+    
+    if (n > 1)
+    {
+        result = true;
+        
+        for (int64_t i = 2; i < n; ++i)
+        {
+            if (!(n % i))
+            {
+                result = false;
+                break;
+            }
+        }
+    }
+    
+    return result;
+}
+
+int64_t SumOfPrimesBelow(int64_t n)
+{
+    int64_t sum = 0;
+    
+    for (int64_t i = 2; i < n; ++i)
+    {
+        if (IsPrime(i)) sum += i;
+    }
+    
+    return sum;
+}
+
+int main(int argc, char** argv)
+{
+    cout << "Sum of primes below 10: "
+        << SumOfPrimesBelow(10) << endl;
+        
+    cout << "Sum of primes below 2,000,000: "
+        << SumOfPrimesBelow(2000000) << endl;
+
+    return 0;
+}

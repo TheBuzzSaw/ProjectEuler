a37e0da7e6aa1b35444a2bfbc3cf23b4dc81cf1a
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2014-03-13 13:03:42 -0600

    Optimized problem 10 moar for good measure.

--- source/010/main.cpp
+++ source/010/main.cpp
@@ -8,9 +8,14 @@ int64_t SumOfPrimesBelow(int64_t n)
 {
     int64_t sum = 0;
     
-    for (int64_t i = 2; i < n; ++i)
+    if (n > 2)
     {
-        if (cache.IsPrime(i)) sum += i;
+        sum = 2;
+        
+        for (int64_t i = 3; i < n; i += 2)
+        {
+            if (cache.IsPrime(i)) sum += i;
+        }
     }
     
     return sum;

76897504a5c4a203911c9f4fb502a6e0e5696f04
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2015-02-25 11:03:13 -0700

    Fixed GetRadians.

--- source/456/main.cpp
+++ source/456/main.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
+#include <vector>
 #include <cstdint>
 #include <cmath>
 using namespace std;
 
-struct Point
-{
-    int16_t x;
-    int16_t y;
-};
-
 template<typename T>
 constexpr T Pi() { return 3.1415926535897932384626433832795; }
+
+template<typename T>
+constexpr T Tau() { return Pi<T>() * T(2); }
 
 template<typename T>
 constexpr T DegreesPerRadian() { return T(180) / Pi<T>(); }
@@ -19,9 +17,37 @@ template<typename T>
 constexpr T ToDegrees(T radians) { return radians * DegreesPerRadian<T>(); }
 
 template<typename T>
-T GetRadians(int x, int y)
+T GetRadians(int64_t x, int64_t y)
 {
-    return atan(T(y) / T(x));
+    if (x == 0)
+    {
+        if (y > 0)
+            return Pi<T>() / T(2);
+        else
+            return Pi<T>() / T(-2);
+    }
+    
+    if (y == 0)
+    {
+        if (x > 0)
+            return 0;
+        else
+            return -Pi<T>();
+    }
+    
+    auto angle = atan(T(y) / T(x));
+    
+    if (x > 0)
+    {
+        return angle;
+    }
+    else
+    {
+        if (y > 0)
+            return Pi<T>() + angle;
+        else
+            return -Pi<T>() + angle;
+    }
 }
 
 void TestGet(int x, int y)
@@ -32,24 +58,60 @@ void TestGet(int x, int y)
     cout << x << ", " << y << " -- " << degrees << endl;
 }
 
-int main(int argc, char** argv)
+int64_t FindC(int64_t count)
 {
     int64_t x = 1;
     int64_t y = 1;
     
-    cout << "P = {";
+    int64_t originCount = 0;
+    vector<double> angles;
+    angles.reserve(count);
     
-    for (int i = 0; i < 8; ++i)
+    for (int i = 0; i < count; ++i)
     {
         x = (x * 1248) % 32323;
         y = (y * 8421) % 30103;
         
-        if (i > 0) cout << ", ";
+        int64_t px = x - 16161;
+        int64_t py = y - 15051;
         
-        cout << "(" << (x - 16161) << ", " << (y - 15051) << ")";
+        if (px == 0 || py == 0)
+            cout << px << ", " << py << endl;
+        
+        if (px == 0 && py == 0)
+            ++originCount;
+        else
+            angles.push_back(GetRadians<double>(px, py));
     }
     
-    cout << "}" << endl;
+    cout << "origin count: " << originCount << endl;
+    cout << "angle count: " << angles.size() << endl;
+    
+    return 0;
+}
+
+void TestGets()
+{
+    TestGet(-1, 50);
+    TestGet(0, 50);
+    TestGet(1, 50);
+    
+    TestGet(50, 1);
+    TestGet(50, 0);
+    TestGet(50, -1);
+    
+    TestGet(1, -50);
+    TestGet(0, -50);
+    TestGet(-1, -50);
+    
+    TestGet(-50, -1);
+    TestGet(-50, 0);
+    TestGet(-50, 1);
+}
+
+int main(int argc, char** argv)
+{
+    FindC(2000000);
     
     return 0;
 }

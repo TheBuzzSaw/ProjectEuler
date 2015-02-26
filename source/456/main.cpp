c9d61e0c88eb87819748799825075596d8ffb694
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2015-02-26 14:10:24 -0700

    Welcome our integer overlords.

--- source/456/main.cpp
+++ source/456/main.cpp
@@ -6,58 +6,34 @@
 #include <cmath>
 using namespace std;
 
-template<typename T>
-constexpr T Pi() { return 3.1415926535897932384626433832795; }
-
-template<typename T>
-constexpr T Tau() { return Pi<T>() * T(2); }
-
-template<typename T>
-constexpr T DegreesPerRadian() { return T(180) / Pi<T>(); }
-
-template<typename T>
-constexpr T ToDegrees(T radians) { return radians * DegreesPerRadian<T>(); }
-
-template<typename T>
-T GetRadians(int64_t x, int64_t y)
+struct Point
 {
-    if (x == 0)
-    {
-        if (y > 0)
-            return Pi<T>() / T(2);
-        else
-            return Pi<T>() / T(-2);
-    }
+    int64_t x;
+    int64_t y;
+};
+
+int GetQuadrant(const Point& p)
+{
+    if (p.y <= 0 && p.x < 0)
+        return 1;
+    else if (p.y < 0 && p.x >= 0)
+        return 2;
+    else if (p.y >= 0 && p.x > 0)
+        return 3;
     
-    if (y == 0)
-    {
-        if (x > 0)
-            return 0;
-        else
-            return -Pi<T>();
-    }
-    
-    auto angle = atan(T(y) / T(x));
-    
-    if (x > 0)
-    {
-        return angle;
-    }
-    else
-    {
-        if (y > 0)
-            return Pi<T>() + angle;
-        else
-            return -Pi<T>() + angle;
-    }
+    return 4; // p.y > 0 && p.x <= 0
 }
 
-void TestGet(int x, int y)
+int64_t CrossZ(const Point& a, const Point& b)
 {
-    auto radians = GetRadians<double>(x, y);
-    auto degrees = ToDegrees(radians);
-    
-    cout << x << ", " << y << " -- " << degrees << endl;
+    return a.x * b.y - a.y * b.x;
+}
+
+bool operator<(const Point& a, const Point& b)
+{
+    auto qa = GetQuadrant(a);
+    auto qb = GetQuadrant(b);
+    return qa < qb || (qa == qb && CrossZ(a, b) > 0);
 }
 
 int64_t FindC(int64_t count)
@@ -69,23 +45,20 @@ int64_t FindC(int64_t count)
     cout.flush();
     
     int64_t originCount = 0;
-    vector<double> angles;
-    angles.reserve(count);
+    vector<Point> points;
+    points.reserve(count);
     
     for (int i = 0; i < count; ++i)
     {
         x = (x * 1248) % 32323;
         y = (y * 8421) % 30103;
         
-        int64_t px = x - 16161;
-        int64_t py = y - 15051;
+        Point p = { x - 16161, y - 15051 };
         
-        //if (count < 10) cout << px << ", " << py << endl;
-        
-        if (px == 0 && py == 0)
+        if (p.x == 0 && p.y == 0)
             ++originCount;
         else
-            angles.push_back(GetRadians<double>(px, py));
+            points.push_back(p);
     }
     
     if (originCount > 0)
@@ -94,84 +67,18 @@ int64_t FindC(int64_t count)
     cout << "Sorting...";
     cout.flush();
     
-    sort(angles.begin(), angles.end());
+    sort(points.begin(), points.end());
     
-    for (size_t i = 1; i < angles.size(); ++i)
-    {
-        if (angles[i] < angles[i - 1])
-            cerr << "THE CAKE IS A LIE.\n";
-    }
+    cout << "\nPOINTS\n";
     
-    cout << "Counting...";
-    cout.flush();
+    for (const auto& point : points)
+        cout << point.x << ", " << point.y << endl;
     
-    int64_t result = 0;
-    
-    auto first = lower_bound(
-        angles.begin(),
-        angles.end(),
-        0.0);
-    
-    for (auto i = angles.begin();
-        i != angles.end() && *i < 0.0;
-        ++i)
-    {
-        auto low = *i + Pi<double>();
-        
-        while (first != angles.end() && *first <= low) ++first;
-        
-        auto f = first;
-        auto k = f;
-        
-        int64_t batch = 0;
-        
-        for (auto j = i + 1;
-            j != angles.end() && *j < low;
-            ++j)
-        {
-            auto high = *j + Pi<double>();
-            
-            if (f <= j)
-            {
-                cout << 'x';
-                cout.flush();
-                f = j + 1;
-                k = f;
-                batch = 0;
-            }
-            
-            while (k != angles.end() && *k < high) ++batch, ++k;
-            
-            result += batch;
-        }
-    }
-    
-    return result;
-}
-
-void TestGets()
-{
-    cout << "TOP\n";
-    for (int i = 0; i < 10; ++i)
-        TestGet(-50 + 10 * i, 50);
-    
-    cout << "RIGHT\n";
-    for (int i = 0; i < 10; ++i)
-        TestGet(50, 50 - 10 * i);
-    
-    cout << "BOTTOM\n";
-    for (int i = 0; i < 10; ++i)
-        TestGet(50 - 10 * i, -50);
-    
-    cout << "LEFT\n";
-    for (int i = 0; i < 10; ++i)
-        TestGet(-50, -50 + 10 * i);
+    return 0;
 }
 
 int main(int argc, char** argv)
 {
-    if (argc < 2) TestGets();
-    
     for (int i = 1; i < argc; ++i)
     {
         int64_t n;

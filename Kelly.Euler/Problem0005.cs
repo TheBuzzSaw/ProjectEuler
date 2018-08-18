3b8bf21edb3213e0f8da88ae2112b7fb4d10ab1e
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2018-08-17 20:41:13 -0600

    Fixed names.

--- Kelly.Euler/Problem0005.cs
+++ Kelly.Euler/Problem0005.cs
@@ -14,33 +14,34 @@ namespace Kelly.Euler
         public override void SolveProblem()
         {
             int number = _number;
-            var maxFactorCount = new short[number - 1];
+            var maxFactorCounts = new short[number - 1];
 
             for (int i = 2; i <= number; ++i)
             {
-                Span<short> factorCount = stackalloc short[maxFactorCount.Length];
+                Span<short> factorCounts = stackalloc short[maxFactorCounts.Length];
 
                 int remainder = i;
                 for (int j = 2; 2 <= remainder; ++j)
                 {
                     if ((remainder % j) == 0)
                     {
-                        ++factorCount[j - 2];
+                        ++factorCounts[j - 2];
                         remainder /= j;
                         j = 1;
                     }
                 }
 
-                for (int j = 0; j < maxFactorCount.Length; ++j)
-                    maxFactorCount[j] = Math.Max(maxFactorCount[j], factorCount[j]);
+                for (int j = 0; j < maxFactorCounts.Length; ++j)
+                    maxFactorCounts[j] = Math.Max(maxFactorCounts[j], factorCounts[j]);
             }
 
             int result = 1;
 
-            for (int i = 0; i < maxFactorCount.Length; ++i)
+            for (int i = 0; i < maxFactorCounts.Length; ++i)
             {
                 int n = i + 2;
-                for (int j = 0; j < maxFactorCount[i]; ++j)
+
+                for (short j = 0; j < maxFactorCounts[i]; ++j)
                     result *= n;
             }
 
